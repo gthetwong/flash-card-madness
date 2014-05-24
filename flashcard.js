b176339844075995894c72c0276cd742acd134c6
gthetwong <gthetwong@gmail.com>
2014-05-24 13:24:19 -0700

    add jsonp request to local json file to grab spanish to english info. also add initial model and collection setup.

--- flashcard.js
+++ flashcard.js
@@ -1,4 +1,7 @@
 
+//////////////////////////////////////
+// CONFIGURATION AND INITIALIZATION //
+//////////////////////////////////////
 window.flashCardApp = {
   Models: {},
   Collections: {},
@@ -8,8 +11,21 @@ window.flashCardApp = {
     this.router = new this.Routers.Main();
     Backbone.history.start({ pushState: true });
     console.log("this is running the pushState");
+    $.ajax({
+      type:'Get',
+      url: 'flashcard_input.json',
+      async: false,
+      jsonpCallback: 'spanishData',
+      contentType: 'application/json',
+      dataType: 'jsonp',
+      success: function(json) {
+        console.log(json,"this is the json");
+      },
+      error: function(e) {
+        console.log(e.message);
+      }
+    });
   }
-
 };
 
 $(function(){
@@ -17,15 +33,43 @@ $(function(){
 });
 
 
+
+/////////////////////////////////////
+//      MODELS AND COLLECTIONS     //
+/////////////////////////////////////
+
+flashCardApp.Models.EnglishWord = Backbone.Model.extend({
+
+});
+
+flashCardApp.Models.SpanishWord = Backbone.Model.extend({
+
+});
+
+flashCardApp.Collections.SpanishWords = Backbone.Collection.extend({
+  model: flashCardApp.Models.SpanishWord
+});
+
+flashCardApp.Collections.EnglishWords = Backbone.Collection.extend({
+  model: flashCardApp.Models.EnglishWord
+});
+
+/////////////////////////////////////
+//             THE VIEWS           //
+/////////////////////////////////////
+
+
 flashCardApp.Views.Home = Backbone.View.extend({
-  template: 'index'
   render: function(){
     this.$el.html("Hellooo Backbone World");
     return this;
   }
-
 });
 
+
+/////////////////////////////////////
+//             THE ROUTER          //
+/////////////////////////////////////
 
 flashCardApp.Routers.Main = Backbone.Router.extend({
   routes: {
@@ -34,6 +78,7 @@ flashCardApp.Routers.Main = Backbone.Router.extend({
   },
   index: function(){
     var home = new flashCardApp.Views.Home();
+    
     $('#spa').html(home.render().$el);
   },
   alert: function(){
